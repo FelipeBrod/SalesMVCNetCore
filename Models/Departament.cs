9414d5ecfeeb58a135e92e178bf9bac2ad311739
Felipe <beraldo.felipe@Gmail.com>
2020-05-04 15:30:31 -0400

    Other entities and second migration

--- Models/Departament.cs
+++ Models/Departament.cs
@@ -9,5 +9,25 @@ namespace SalesWebMvc.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(int iD, string name)
+        {
+            ID = iD;
+            Name = name;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
